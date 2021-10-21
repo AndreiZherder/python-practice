03ada6fab7851406166ce873c28bd7b7591fee7b
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-10-21 15:06:11 +0300

    file - part 2

---
+++ file2.py
@@ -0,0 +1,39 @@
+# Недавно мы считали для каждого слова количество его вхождений в строку.
+# Но на все слова может быть не так интересно смотреть, как, например, на наиболее часто используемые.
+#
+# Напишите программу, которая считывает текст из файла (в файле может быть больше одной строки)
+# и выводит самое частое слово в этом тексте и через пробел то, сколько раз оно встретилось.
+# Если таких слов несколько, вывести лексикографически первое (можно использовать оператор < для строк).
+#
+# В качестве ответа укажите вывод программы, а не саму программу.
+# Слова, написанные в разных регистрах, считаются одинаковыми.
+
+# Sample Input:
+# abc a bCd bC AbC BC BCD bcd ABC
+#
+# Sample Output:
+# abc 3
+
+inf = open('./data/Bible.txt', 'r')
+ouf = open('./data/out.txt', 'w')
+word_met = dict()
+for in_string in inf:
+    for i in range(len(in_string) - 1, -1, -1):
+        if not in_string[i].isalnum() and in_string[i] != ' ':
+            in_string = in_string[:i] + in_string[i + 1:]
+    in_string = in_string.strip()
+    ouf.write(f'{in_string}\n')  # в добавление к задаче в файл записываются строки без служебных символов
+    words = in_string.lower().split()
+    for word in words:
+        if word not in word_met:
+            word_met[word] = 1
+        else:
+            word_met[word] += 1
+max_times_word_met = max(word_met.values())
+most_met_words = [word for word in word_met.keys() if word_met[word] == max_times_word_met]
+word = min(most_met_words)
+for word, times_word_met in sorted(word_met.items(), key=lambda item: (item[1], item[0])):
+    ouf.write(f'{word} {times_word_met}\n')  # в добавление к задаче в файл записываютя слова и их количество
+ouf.write(f'{word} {max_times_word_met}\n')
+ouf.close()
+inf.close()
---
+++ file3.py
@@ -0,0 +1,51 @@
+# Имеется файл с данными по успеваемости абитуриентов.
+# Он представляет из себя набор строк, где в каждой строке записана следующая информация:
+# Фамилия;Оценка_по_математике;Оценка_по_физике;Оценка_по_русскому_языку
+#
+# Поля внутри строки разделены точкой с запятой, оценки — целые числа.
+# Напишите программу, которая считывает исходный файл с подобной структурой
+# и для каждого абитуриента записывает его среднюю оценку по трём предметам на отдельной строке,
+# соответствующей этому абитуриенту, в файл с ответом.
+#
+# Также вычислите средние баллы по математике,
+# физике и русскому языку по всем абитуриентам и добавьте полученные значения, разделённые пробелом,
+# последней строкой в файл с ответом.
+#
+# В качестве ответа на задание прикрепите полученный файл со средними оценками по каждому ученику
+# и одной строкой со средними оценками по трём предметам.
+
+# Sample Input:
+#
+# Петров;85;92;78
+# Сидоров;100;88;94
+# Иванов;58;72;85
+#
+# Sample Output:
+#
+# 85.0
+# 94.0
+# 71.666666667
+# 81.0 84.0 85.666666667
+
+grades = []
+with open('./data/dataset_3363_4.txt', 'r', encoding='utf-8') as inf:
+    for line in inf:
+        applicant = line.split(';')
+        grades.append(applicant)
+sum_of_math_grades = 0
+sum_of_physics_grades = 0
+sum_of_russian_grades = 0
+with open('./data/out.txt', 'w', encoding='utf-8') as ouf:
+    for applicant in grades:
+        sum_of_applicant_grades = 0
+        for grade in applicant[1:]:
+            sum_of_applicant_grades += int(grade)
+        mean = sum_of_applicant_grades / (len(applicant) - 1)
+        ouf.write(f'{mean}\n')
+        sum_of_math_grades += int(applicant[1])
+        sum_of_physics_grades += int(applicant[2])
+        sum_of_russian_grades += int(applicant[3])
+    mean_of_math_grades = sum_of_math_grades / len(grades)
+    mean_of_physics_grades = sum_of_physics_grades / len(grades)
+    mean_of_russian_grades = sum_of_russian_grades / len(grades)
+    ouf.write(f'{mean_of_math_grades} {mean_of_physics_grades} {mean_of_russian_grades}\n')
