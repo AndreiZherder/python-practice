cad6b37cc249f6077c4486f843b78ff1b56d589b
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-08 23:30:48 +0300

    module - part 2

---
+++ stepik512/module2.py
@@ -0,0 +1,37 @@
+"""
+Алиса владеет интересной информацией, которую хочет заполучить Боб.
+Алиса умна, поэтому она хранит свою информацию в зашифрованном файле.
+У Алисы плохая память, поэтому она хранит все свои пароли в открытом виде в текстовом файле.
+
+Бобу удалось завладеть зашифрованным файлом с интересной информацией и файлом с паролями,
+но он не смог понять какой из паролей ему нужен. Помогите ему решить эту проблему.
+
+Алиса зашифровала свою информацию с помощью библиотеки simple-crypt.
+Она представила информацию в виде строки, и затем записала в бинарный файл результат работы метода simplecrypt.encrypt.
+
+Вам необходимо установить библиотеку simple-crypt, и с помощью метода simplecrypt.decrypt узнать,
+какой из паролей служит ключом для расшифровки файла с интересной информацией.
+
+Ответом для данной задачи служит расшифрованная интересная информация Алисы.
+
+Файл с информацией
+Файл с паролями
+
+Примечание:
+Для того, чтобы считать все данные из бинарного файла, можно использовать, например, следующий код:
+
+with open("encrypted.bin", "rb") as inp:
+    encrypted = inp.read()
+"""
+import simplecrypt
+import requests
+encrypted = requests.get('https://stepik.org/media/attachments/lesson/24466/encrypted.bin')
+passwords = requests.get('https://stepik.org/media/attachments/lesson/24466/passwords.txt')
+for password in passwords.text.splitlines():
+    try:
+        text = simplecrypt.decrypt(password, encrypted.content).decode('utf8')
+        print(text)
+    except simplecrypt.DecryptionException:
+        print(f'Password {password} raised DecryptionException')
+
+
