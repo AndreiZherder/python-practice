f8cc31fdb144a375d64e9bcc3d8823501a81b956
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-18 23:51:36 +0300

    collections - part 1

---
+++ stepik512/collections1.py
@@ -0,0 +1,14 @@
+import collections
+import random
+
+
+lst = list(random.randrange(0, 10) for i in range(50))
+print(lst)
+cnt = collections.Counter(lst)
+print(cnt)
+print(cnt.most_common(1))
+
+d = collections.defaultdict(list)
+for i in range(10):
+    d[i].extend([i, i * i, i * i * i])
+print(d)
