596fb5fc824ca7bfaccb2cdefed426e0e351151d
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-18 20:19:25 +0300

    iterator_generator - part 6 - itertools

--- stepik512/iterator_generator7.py
+++ stepik512/iterator_generator7.py
@@ -1,5 +1,16 @@
 import itertools
 
-with open('../data/Bible.txt', 'r') as f:
-    for pos, line in itertools.islice(enumerate(f), 500, 1000):
-        print(pos, line)
+lst = list(range(10))
+print(*lst)
+print(*map(lambda x, y: x * y, lst, itertools.repeat(2)))
+print(*map(lambda x, y: x * y, lst, itertools.cycle([1, -1])))
+print(*map(lambda x, y: x * y, lst, itertools.count()))
+print(*itertools.accumulate(lst))
+print(*itertools.chain(lst, range(9, -1, -1)))
+print(*filter(lambda x: x % 2 == 0, lst))
+print(*itertools.filterfalse(lambda x: x % 2 == 0, lst))
+print(*itertools.compress(lst, (0 if i % 2 == 1 else 1 for i in range(10))))
+print(*itertools.dropwhile(lambda x: x < 5, lst))
+print(*itertools.takewhile(lambda x: x < 5, lst))
+
+
