62b0e2fd8d43293c28cdc0ada6522ba6f8229e00
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-18 10:23:49 +0300

    json - part 3

--- stepik512/json2.py
+++ stepik512/json2.py
@@ -1,24 +1,23 @@
 import json
 
-s = '{' \
-    '    "firstName": "Иван",' \
-    '    "lastName": "Иванов",' \
-    '    "address": {' \
-    '        "streetAddress": "Московское ш., 101, кв.101",' \
-    '        "city": "Ленинград",' \
-    '        "postalCode": 101101' \
-    '    },' \
-    '    "phoneNumbers": [' \
-    '        "812 123-1234",' \
-    '        "916 123-4567"' \
-    '    ]' \
-    '}'
+s = {
+        "firstName": "Иван",
+        "lastName": "Иванов",
+        "address": {
+            "streetAddress": "Московское ш., 101, кв.101",
+            "city": "Ленинград",
+            "postalCode": 101101
+       },
+        "phoneNumbers": [
+            "812 123-1234",
+            "916 123-4567"
+        ]
+}
 
 
 def main():
-    j = json.loads(s)
-    for k, v in j.items():
-        print(f'{k}: {v}')
+    j = json.dumps(s, ensure_ascii=False)
+    print(j)
 
 
 if __name__ == '__main__':
