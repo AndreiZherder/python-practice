fede1357420d0d67d3675d5a92aeb0fde1e928ae
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-23 15:07:46 +0300

    exercises - part 6

---
+++ stepik431/2.1.py
@@ -0,0 +1,17 @@
+"""
+What will the following program print out?
+
+x = 43
+x = x + 1
+print(x)
+"""
+
+
+def main():
+    x = 43
+    x = x + 1
+    print(x)
+
+
+if __name__ == '__main__':
+    main()
