3b5928707bd4c43437f06cbca259d50018a3fb57
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-02-13 19:53:52 +0300

    decorator

---
+++ stepik512/decorator.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def trace(s1, s2):
+    def deco(f):
+        def wrapper(*args, **kwargs):
+            print(s1)
+            res = f(*args, **kwargs)
+            print(s2)
+            return res
+        return wrapper
+    return deco
+
+
+def main():
+    # @trace(s1='before', s2='after')
+    def min(a: List[int]) -> int:
+        ans = float('Inf')
+        for num in a:
+            if num < ans:
+                ans = num
+        return ans
+
+    a = [1, 2, 3, 4, 5]
+    deco = trace(s1='before', s2='after')
+    min = deco(min)
+    print(min(a))
+
+
+if __name__ == '__main__':
+    main()
