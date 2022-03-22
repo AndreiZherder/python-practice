c7949b05456b4151ad8b66bd617495e020280ce2
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-03-23 00:00:33 +0300

    some fixes

--- async/heap-event-loop.py
+++ async/heap-event-loop.py
@@ -2,17 +2,21 @@ import heapq
 import time
 
 
+def sleep(delay):
+    yield time.time() + delay
+
+
 def rhythm():
     print('1')
     for i in range(10):
-        yield time.time() + 1
+        yield from sleep(1)
         print('1')
 
 
 def bang():
     print('Bang')
     for i in range(10):
-        yield time.time() + 3
+        yield from sleep(3)
         print('Bang')
 
 
@@ -25,7 +29,9 @@ def main():
     heapq.heappush(tasks, (time.time(), priority[task2], task2))
     while tasks:
         start_time, _, task = heapq.heappop(tasks)
-        time.sleep(start_time - time.time())
+        wait = start_time - time.time()
+        if wait > 0:
+            time.sleep(wait)
         try:
             start_time = next(task)
         except StopIteration:
