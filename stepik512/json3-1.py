bc66d3569408caaa56499ee20895e12ab9d29375
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2023-02-07 17:50:24 +0300

    requests revision

---
+++ stepik512/json3-1.py
@@ -0,0 +1,47 @@
+"""
+В этой задаче вам необходимо воспользоваться API сайта numbersapi.com
+
+Вам дается набор чисел.
+Для каждого из чисел необходимо узнать, существует ли интересный математический факт об этом числе.
+
+Для каждого числа выведите Interesting, если для числа существует интересный факт, и Boring иначе.
+Выводите информацию об интересности чисел в таком же порядке, в каком следуют числа во входном файле.
+
+Пример запроса к интересному числу:
+http://numbersapi.com/31/math?json=true
+
+Пример запроса к скучному числу:
+http://numbersapi.com/999/math?json=true
+
+Пример входного файла:
+31
+999
+1024
+502
+
+Пример выходного файла:
+Interesting
+Boring
+Interesting
+Boring
+
+У вас есть неограниченное число попыток.
+Время одной попытки: 5 mins
+"""
+import requests
+
+
+def main():
+    filename = '../data/dataset_24476_3.txt'
+    with open(filename) as file:
+        nums = (int(num) for num in file)
+        for num in nums:
+            response = requests.get(f'http://numbersapi.com/{num}/math', params={'json': True})
+            if response.json()['found']:
+                print('Interesting')
+            else:
+                print('Boring')
+
+
+if __name__ == '__main__':
+    main()
